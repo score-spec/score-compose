13a5a0804c36f0621e60530bb4475ff16f84c1ed
rachfop <prachford@icloud.com>
2022-10-20 14:43:57 -0700

    Add headers in all go files
    Signed-off-by: rachfop <prachford@icloud.com>

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -1,3 +1,10 @@
+/*
+Apache Score
+Copyright 2020 The Apache Software Foundation
+
+This product includes software developed at
+The Apache Software Foundation (http://www.apache.org/).
+*/
 package cmd
 
 import (
