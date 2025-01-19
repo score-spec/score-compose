ea289977261276aa80622cf97d4aadb9dece97f4
lekaf974 <matthieu.evrin@gmail.com>
2025-01-18 21:19:22 -0500

    feat: add provisioners list command
    Signed-off-by: lekaf974 <matthieu.evrin@gmail.com>

---
+++ internal/command/provisioners.go
@@ -0,0 +1,102 @@
+// Copyright 2024 Humanitec
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package command
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/score-spec/score-compose/internal/project"
+	"github.com/score-spec/score-compose/internal/provisioners/loader"
+)
+
+var (
+	provisionersGroup = &cobra.Command{
+		Use:   "provisioners",
+		Short: "Subcommands related to provisioners",
+	}
+	provisionersList = &cobra.Command{
+		Use:   "list",
+		Short: "List the provisioners",
+		Long: `The list command will list out the provisioners. This requires an active score compose state
+after 'init' or 'generate' has been run. The list of provisioners will be empty if no provisioners are defined.
+`,
+		Args:          cobra.ArbitraryArgs,
+		SilenceErrors: true,
+		RunE:          listProvisioners,
+	}
+)
+
+func listProvisioners(cmd *cobra.Command, args []string) error {
+	cmd.SilenceUsage = true
+	wd, _ := os.Getwd()
+	sd, ok, err := project.LoadStateDirectory(wd)
+	if err != nil {
+		return fmt.Errorf("failed to load existing state directory: %w", err)
+	} else if !ok {
+		return fmt.Errorf("no state directory found, run 'score-compose init' first")
+	}
+	slog.Info(fmt.Sprintf("Listing provisioners in project '%s'", sd.State.Extras.ComposeProjectName))
+	provisionerFilePath, err := getProvisionerFile(sd.Path, sd.State.Extras.ComposeProjectName)
+	if err != nil {
+		return fmt.Errorf("failed to get provisioner file: %w", err)
+	}
+	provisionerContent, err := os.ReadFile(provisionerFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to read provisioner file: %w", err)
+	}
+	loadedProvisioners, err := loader.LoadProvisioners(provisionerContent)
+	if err != nil {
+		return fmt.Errorf("failed to load provisioners: %w", err)
+	}
+	for _, provisioner := range loadedProvisioners {
+		slog.Info(fmt.Sprintf("Provisioner: %s", provisioner.Uri()))
+	}
+	return nil
+}
+
+// getProvisionerFile returns the path to the provisioner file matching the given hash,
+// or falls back to the default file if no match is found
+func getProvisionerFile(path string, projectName string) (string, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return "", fmt.Errorf("failed to read directory: %w", err)
+	}
+
+	slog.Debug(fmt.Sprintf("Looking for provisioner file for project '%s' in path '%s'", projectName, path))
+
+	// Look for a file matching the hash
+	for _, entry := range entries {
+		if !entry.IsDir() && strings.HasPrefix(entry.Name(), "Z-") && strings.HasSuffix(entry.Name(), ".provisioners.yaml") {
+			return filepath.Join(path, entry.Name()), nil
+		}
+	}
+	// Fall back to default file
+	defaultFile := filepath.Join(path, "zz-default.provisioners.yaml")
+	if _, err := os.Stat(defaultFile); err != nil {
+		return "", fmt.Errorf("default provisioners file not found: %w", err)
+	}
+	return defaultFile, nil
+}
+
+func init() {
+	provisionersGroup.AddCommand(provisionersList)
+	rootCmd.AddCommand(provisionersGroup)
+}
